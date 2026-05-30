63e12bc89fc80118707b2f76fd6f61222ac566cd
deepak rawat <deepak.rawat@quokkalabs.com>
2026-05-30 23:18:44 +0530

    fix: main

--- Two.js
+++ Two.js
@@ -1,4 +1,5 @@
 function two() { 
     // This change by deepak rawat to check conflicts in this branch 
     console.log('testing two fucntion for git basics')
+    // Iam from main
 }
--- one.js
+++ one.js
@@ -1,6 +1,6 @@
 function hey() { 
-    // This change for the new commit by Deepak Rawat
-    
+    // This change for the new commit by Deepak Rawat.
+    // I am from main.
     console.log("Hey there!");
     
 }
